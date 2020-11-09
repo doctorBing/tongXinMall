9d421ec85677177a9956aad7cfeeed57d7b04a53
docterBing <512652188@qq.com>
2020-11-09 22:25:02 +0800

    查找全部分类，封装成树形结构

--- tongxinmall-product/src/main/java/com/ziyin/tongxinmall/product/service/impl/CategoryServiceImpl.java
+++ tongxinmall-product/src/main/java/com/ziyin/tongxinmall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,13 @@
 package com.ziyin.tongxinmall.product.service.impl;
 
+import org.apache.logging.log4j.util.PropertySource;
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -25,5 +31,35 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
 
         return new PageUtils(page);
     }
+
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        // 查出所有分类
+        List<CategoryEntity> entities = baseMapper.selectList(null);
+
+        // 封装成树结构
+        List<CategoryEntity> levelMenu = entities.stream().filter(categoryEntity ->
+                categoryEntity.getParentCid() == 0
+        ).map(menu -> {
+            menu.setChildren(getChildrens(menu, entities));
+            return menu;
+        }).sorted(Comparator.comparingInt(menu -> (menu.getSort() == null ? 0 : menu.getSort()))
+        ).collect(Collectors.toList());
+
+        return levelMenu;
+    }
+
+    private List<CategoryEntity> getChildrens(CategoryEntity parent, List<CategoryEntity> all) {
+        List<CategoryEntity> children = all.stream().filter(categoryEntity ->
+                categoryEntity.getParentCid() == parent.getCatId()
+        ).map(categoryEntity -> {
+            categoryEntity.setChildren(getChildrens(categoryEntity, all));
+            return categoryEntity;
+        }).sorted(Comparator.comparingInt(menu -> (menu.getSort() == null ? 0 : menu.getSort()))
+        ).collect(Collectors.toList());
+
+        return children;
+    }
+
 
 }
